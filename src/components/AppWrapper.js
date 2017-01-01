b358227eb20ce588a95909ff84a853fb286d0eb8
Brad Bohen <bbohen@gmail.com>
2017-01-01 16:40:49 -0500

    Cleanup, prep components for data

---
+++ src/components/AppWrapper.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  display: flex;
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background: Ivory;
+  color: DarkSlateGray;
+`;
+
+export default AppWrapper;
