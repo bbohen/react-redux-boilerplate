fd3d9c8a4a306a0bcb828d6cb72e87c6004d01c1
Brad Bohen <bbohen@gmail.com>
2017-01-08 18:26:20 -0500

    Added test for NotFound

---
+++ __tests__/components/NotFound.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { NotFound } from '../../src/components';
+
+describe('<NotFound />', () => {
+  it('renders the not found component', () => {
+    const component = renderer.create(
+      <NotFound />,
+    );
+    const tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
